d008a702d721fd41fc72973d334eb4c3e8b8561a
whyrupesh <srupeshkumar800@gmail.com>
2024-06-01 01:11:09 +0530

    download cv bug fix

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,7 +7,7 @@ import Skill from "./Skill";
 
 export default function () {
   const onButtonClick = () => {
-    const pdfUrl = "resources/Rupesh Resume.pdf";
+    const pdfUrl = "/static/Rupesh Resume.pdf";
     const link = document.createElement("a");
     link.href = pdfUrl;
     link.download = "Rupesh Resume.pdf"; // specify the filename
