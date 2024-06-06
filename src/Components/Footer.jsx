85d8f8795532f59f7fe4ce9b3b9c000af97f63df
whyrupesh <srupeshkumar800@gmail.com>
2024-06-06 14:13:48 +0530

    added footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { animateScroll as scroll } from "react-scroll";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   const scrollToTop = () => {
@@ -10,6 +11,9 @@ export default function Footer() {
       <button onClick={scrollToTop} className="border-2 m-5 p-2 bg-slate-600">
         Back to Top
       </button>
+      <div className="flex items-center justify-center mb-5">
+        <p>Made with 💖 by @whyrupesh</p>
+      </div>
     </>
   );
 }
